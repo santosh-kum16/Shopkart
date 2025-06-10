83401caceed7e3ec96ab15a2f21a8fce6d5944eb
Santosh Kumar <santosh.kum16@gmail.com>
2025-06-10 23:30:33 +0530

    Default files generated by Spring Boot

---
+++ SpringBoot/Shopkart/src/main/java/com/example/Shopkart/ShopkartApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shopkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopkartApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/Shopkart/src/test/java/com/example/Shopkart/ShopkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shopkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
