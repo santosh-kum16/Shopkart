36cd02b1f5eeea9671abe6079042d29261beefaf
Santosh Kumar <santosh.kum16@gmail.com>
2025-06-13 22:29:50 +0530

    JWT Auth Implementation

---
+++ SpringBoot/Shopkart/src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
