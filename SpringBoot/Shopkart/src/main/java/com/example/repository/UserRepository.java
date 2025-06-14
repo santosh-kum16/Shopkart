b2ad4b0d86cfe709548b3aef44bb7a9285641b05
Santosh Kumar <santosh.kum16@gmail.com>
2025-06-14 16:44:08 +0530

    User Repository Created to find user by email.

---
+++ SpringBoot/Shopkart/src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+	public User findByEmail(String email);
+}
